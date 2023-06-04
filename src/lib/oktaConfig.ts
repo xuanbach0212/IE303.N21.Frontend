ff60d0097362fc5a4d2c6fba267e20ee30ce8dd9
Xuân Bách Đào <xuanbachdao0212@gmail.com>
2023-06-05 00:55:42 +0700

    change okta

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 const oktaConfig = {
-    clientId: '0oa9ix9zf4Jh7E7895d7',
-    issuer: 'https://dev-25284660.okta.com/oauth2/default',
-    redirectUri: 'http://54.251.217.42:3000/login/callback',
+    clientId: '0oa9tr9smuh0Y3V2j5d7',
+    issuer: 'https://dev-01811862.okta.com/oauth2/default',
+    redirectUri: 'http://54.179.229.192:3001/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
